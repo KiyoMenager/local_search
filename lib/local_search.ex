8fe679b37e4e89878012b5edc75b3c5c193d85c8
Kiyo <menager.kiyoaki@gmail.com>
2016-11-06 21:33:06 +0100

    Make project follow callback deps injection struct

--- lib/local_search.ex
+++ lib/local_search.ex
@@ -1,2 +1,16 @@
 defmodule LocalSearch do
+
+  @default_algorithm TwoOpt
+  @algorithm [:two_opt]
+
+  @doc """
+
+  Example usage:
+    LocalSearch.run(encoded_sol, distances_matrix, algo: TwoOpt)
+
+  """
+  def run(encoded_sol, distances_matrix, opts \\ []) do
+    {local_search, opts} = Keyword.pop(opts, :algo, @default_algorithm)
+    local_search.run(encoded_sol, distances_matrix)
+  end
 end
